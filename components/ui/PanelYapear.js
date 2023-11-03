a5287d222e33686d7401d1c00af8734da88218d6
Luis Santos <luisdemaryori@gmail.com>
2023-11-03 17:02:56 -0500

    mejora000020

--- components/ui/PanelYapear.js
+++ components/ui/PanelYapear.js
@@ -6,31 +6,8 @@ import SliderBilletera from "./SliderBilletera";
 import DatosBilletera from "./DatosBilletera";
 import Link from "next/link";
 import SpinnerHistorial from "./SpinnerHistorial";
-import recuperarNumeros from "@/Validacion/recuperarNumeros";
-import PanelNumero from "./PanelNumero";
-
-const BuscarTelefono = styled.div`
-  height: 8%;
-  .contenedorTelefono {
-    position: relative;
-    padding: 0 20px;
-    display: flex;
-    align-items: center;
-  }
-  i {
-    position: absolute;
-    color: black;
-    margin-left: 2px;
-  }
-  input {
-    width: 100%;
-    height: 35px;
-    padding-left: 20px;
-    padding-right: 5px;
-    font-size: 12px;
-    border-radius: 5px;
-  }
-`;
+import recuperarDatos from "@/Validacion/recuperarDatos";
+import transferirDineroYape from "@/Validacion/transferirDineroYape";
 const MostrarError = styled.div`
   width: 300px;
   height: 505px;
@@ -64,6 +41,46 @@ const Contenedor = styled.div`
   height: 500px;
   width: 300px;
   border-radius: 15px;
+  .datosGenerales {
+    text-align: center;
+    font-weight: bold;
+    span,
+    p {
+      font-size: 25px;
+    }
+  }
+  form {
+    padding: 20px;
+    button {
+      background-color: var(--botonesBilletera);
+      width: 100%;
+      height: 50px;
+      border: none;
+      border-radius: 10px;
+      margin-top: 20px;
+      color: white;
+      font-size: 20px;
+      font-weight: bold;
+      cursor: pointer;
+    }
+  }
+  .contenedorValor {
+    input {
+      height: 100%;
+      width: 100%;
+      background-color: transparent;
+      border: none;
+      color: white;
+      font-size: 60px;
+      text-align: center;
+      :focus {
+        outline: none;
+      }
+      ::placeholder {
+        color: white;
+      }
+    }
+  }
   .encabezado {
     padding: 10px;
     background-color: var(--botonesBilletera);
@@ -128,15 +145,16 @@ const Contenedor = styled.div`
     }
   }
 `;
-const PanelTransferencia = ({ token }) => {
+const PanelYapear = ({ token, id }) => {
   const { firebase, usuario } = useContext(FirebaseContext);
-  const [datosUser, setDatos] = useState();
   const [mostrarSaldo, setMostrarSaldo] = useState(false);
+  const [datosUser, setDatos] = useState();
   const [saldo, setSaldo] = useState(0);
   const [historiales, setHistoriales] = useState();
   const [loading, setLoading] = useState(true); // Estado de carga
   const [error, setError] = useState();
-  const [numero, setNumero] = useState();
+  const [valor, setValor] = useState();
+  const [respuesta, setRespuesta] = useState();
   const retornoSaldo = () => {
     if (mostrarSaldo) {
       setMostrarSaldo(false);
@@ -150,64 +168,67 @@ const PanelTransferencia = ({ token }) => {
       currency: "PEN",
     });
   };
-  const cargarDatos = (e) => {
-    setNumero(e.target.value);
-  };
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         "https://billapp-5d53d479ff62.herokuapp.com/api/wallet",
-  //         {
-  //           method: "GET",
-  //           headers: {
-  //             Authorization: `Bearer ${token}`,
-  //           },
-  //         }
-  //       );
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://billapp-5d53d479ff62.herokuapp.com/api/wallet",
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
 
-  //       const data = await response.json();
+        const data = await response.json();
 
-  //       if (!response.ok) {
-  //         throw new Error(data.message || "Error de servidor");
-  //       }
+        if (!response.ok) {
+          throw new Error(data.message || "Error de servidor");
+        }
 
-  //       setHistoriales(data["data"]["WalletHistrial"]);
-  //       setSaldo(data["data"]["cash"]);
-  //       setLoading(false); // Marca como cargado
-  //     } catch (error) {
-  //       // console.error("Error al obtener datos:", error.message);
-  //       if (error.message.length === 29) {
-  //         setError(error.message);
-  //         setLoading(false);
-  //       }
-  //       if (
-  //         (error.message =
-  //           "Cannot read properties of null (reading 'WalletHistrial')")
-  //       ) {
-  //         setLoading(false);
-  //       }
-  //     }
-  //   };
+        setHistoriales(data["data"]["WalletHistrial"]);
+        setSaldo(data["data"]["cash"]);
+        setLoading(false); // Marca como cargado
+      } catch (error) {
+        // console.error("Error al obtener datos:", error.message);
+        if (error.message.length === 29) {
+          setError(error.message);
+          setLoading(false);
+        }
+        if (
+          (error.message =
+            "Cannot read properties of null (reading 'WalletHistrial')")
+        ) {
+          setLoading(false);
+        }
+      }
+    };
 
-  //   fetchData(); // Llama a la función asincrónica
-  // }, [token]);
+    fetchData(); // Llama a la función asincrónica
+  }, [token]);
 
   useEffect(() => {
     const llamadaDatos = async () => {
-      const datos = await recuperarNumeros(numero, token);
-      // console.log(datos["error"]);
-      // console.log("desde", datos["data"]);
+      const datos = await recuperarDatos(id, token);
       setDatos(datos["data"]);
-      if (datos["error"]) {
-        setError(datos["message"]);
-      }
       setLoading(false); // Marca como cargado
     };
     llamadaDatos();
-  }, [numero]);
+  }, []);
 
+  const formatoMoneda = (e) => {
+    const valorNumerico = e.target.value.replace(/[^0-9]/g, "");
+    setValor(valorNumerico === "" ? "" : `S/ ${valorNumerico}`);
+  };
+  const transferirDinero = async (e) => {
+    e.preventDefault();
+    const numeroExtraido = valor.replace(/\D/g, "");
+    const numero = parseInt(numeroExtraido, 10);
+    const datos = await transferirDineroYape(id, numero, token);
+    setRespuesta(datos["message"]);
+  };
   return (
     <div
       css={css`
@@ -230,6 +251,23 @@ const PanelTransferencia = ({ token }) => {
             </div>
           </MostrarError>
         )}
+        {respuesta && (
+          <MostrarError>
+            <div>
+              <span
+                css={css`
+                  text-align: center;
+                  color: #10da1a !important;
+                `}
+              >
+                {respuesta}
+              </span>
+              <Link href={`/usuarios/${token}`}>
+                Regresar a la Pagina Principal
+              </Link>
+            </div>
+          </MostrarError>
+        )}
         <div
           css={css`
             height: 10%;
@@ -242,40 +280,52 @@ const PanelTransferencia = ({ token }) => {
                   <i class="bx bx-x"></i>
                 </Link>
               </div>
-              <div>{usuario && <p>Yapear</p>}</div>
+              <div>{usuario && <p>Yapear a</p>}</div>
             </div>
           </div>
         </div>
-        <BuscarTelefono>
-          <div className="contenedorTelefono">
-            <i class="bx bx-search-alt bx-rotate-90"></i>
-            <input
-              type="text"
-              placeholder="Ingrese el celular"
-              value={numero}
-              onChange={cargarDatos}
-            />
-          </div>
-        </BuscarTelefono>
 
         <div
           css={css`
-            height: 80%;
+            height: 90%;
           `}
         >
           {loading && <SpinnerHistorial />}
+          {datosUser && (
+            <div className="datosGenerales">
+              <span>
+                {datosUser["names"]} {datosUser["lastname"]}
+              </span>
+              <p>{datosUser["phone"]}</p>
+            </div>
+          )}
+          <form onSubmit={transferirDinero}>
+            <div className="contenedorValor">
+              <input
+                type="text"
+                id="monto"
+                value={valor}
+                onChange={formatoMoneda}
+                placeholder="S/ 0"
+              />{" "}
+            </div>
 
-          <ul
+            <div className="contenedorBoton">
+              <button>Yapear</button>
+            </div>
+          </form>
+
+          {/* <ul
             css={css`
               height: 100%;
               margin-top: 5px;
               overflow-y: auto;
             `}
           >
-            {datosUser && datosUser.length != 0 ? (
+            {historiales ? (
               <>
-                {datosUser.map((usuario, index) => (
-                  <PanelNumero key={index} usuario={usuario} token={token} />
+                {historiales.map((historial, index) => (
+                  <DatosBilletera key={index} historial={historial} />
                 ))}
               </>
             ) : (
@@ -287,15 +337,15 @@ const PanelTransferencia = ({ token }) => {
                     padding: 0 20px;
                   `}
                 >
-                  "Sin Resultados"
+                  "Aún no tiene historial"
                 </p>
               )
             )}
-          </ul>
+          </ul> */}
         </div>
       </Contenedor>
     </div>
   );
 };
 
-export default PanelTransferencia;
+export default PanelYapear;
